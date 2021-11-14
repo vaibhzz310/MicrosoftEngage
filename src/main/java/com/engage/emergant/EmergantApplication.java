8a1cc6e312b035b8668f0689486e59c41b7dc4c8
Vaibhav <vaibhav302000@gmail.com>
2021-11-15 03:20:02 +0530

    spring boot initializer

---
+++ src/main/java/com/engage/emergant/EmergantApplication.java
@@ -0,0 +1,13 @@
+package com.engage.emergant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engage/emergant/EmergantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engage.emergant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
